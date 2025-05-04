50c8615f808dc8b9dc1463e7e21790c2fb8d294a
Zeit-bit <reyesalvarez.patricio@gmail.com>
2025-05-04 00:06:21 -0600

    Add routing of auth requests

---
+++ index.js
@@ -0,0 +1,36 @@
+import express from 'express'
+import cors from 'cors'
+
+const app = express()
+const port = 3000
+
+const users = []
+
+app.use(express.json())
+app.use(cors())
+
+app.get('/users', (req, res) => {
+  res.json(users)
+})
+
+app.post('/users', async (req, res) => {
+  const userData = await req.body
+  const user = users.find((user) => user.email === userData.email)
+  if (user) return res.json({ error: 'correo electronico ya en uso' })
+  users.push(userData)
+})
+
+app.post('/login', async (req, res) => {
+  const userData = await req.body
+  const user = users.find((user) => user.email === userData.email)
+  if (!user) return res.json({ error: 'No existe el usuario' })
+  if (user.password !== userData.password) {
+    return res.json({ error: 'Contraseña incorrecta' })
+  }
+
+  res.json(user)
+})
+
+app.listen(port, () => {
+  console.log(`Server listening on port ${port}`)
+})
